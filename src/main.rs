a7acdb451d7cbe446eeed6a76008a9a4fc630100
Artem Demo <artemdemo@gmail.com>
2024-12-27 20:02:02 +0200

    draw first number

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use notan::draw::*;
 use notan::prelude::*;
 
 #[notan_main]
 fn main() -> Result<(), String> {
   notan::init_with(setup)
     .add_config(WindowConfig::default().set_size(700, 300))
+    .add_config(DrawConfig)
     .draw(draw)
     .build()
 }
@@ -11,11 +13,17 @@ fn main() -> Result<(), String> {
 #[derive(AppState)]
 struct State {
   clear_options: ClearOptions,
+  img: Texture,
 }
 
 fn setup(gfx: &mut Graphics) -> State {
-  let clear_options = ClearOptions::color(Color::new(0.3, 0.3, 0.3, 1.0));
-  State { clear_options }
+  let clear_options = ClearOptions::color(Color::new(0.4, 0.4, 0.4, 1.0));
+  let img = gfx
+    .create_texture()
+    .from_image(include_bytes!("assets/num-0-0.png"))
+    .build()
+    .unwrap();
+  State { clear_options, img }
 }
 
 fn draw(gfx: &mut Graphics, state: &mut State) {
@@ -25,4 +33,8 @@ fn draw(gfx: &mut Graphics, state: &mut State) {
   renderer.end();
 
   gfx.render(&renderer);
+
+  let mut draw = gfx.create_draw();
+  draw.image(&state.img).position(0.0, 0.0).scale(0.2, 0.2);
+  gfx.render(&draw);
 }
